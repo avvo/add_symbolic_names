6b3be6fb0a7d110f9028ee4751dfe59d0e11a2d4
Jeff Ching <jching@avvo.com>
2013-09-17 20:50:13 -0700

    add test framework

---
+++ test/add_symbolic_names_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddSymbolicNamesTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, AddSymbolicNames
+  end
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'd5bc6565caee16af88839eee2403d40295506b9f74abb22015d9f68fc8e03a393caf5dab6888921a236f9e23b03d685f3f44c15dc507826b037faca8780bcefe'
