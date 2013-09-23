6e85bc4570939a24c1840e44d7776fe25d84735d
Jeff Ching <jching@avvo.com>
2013-09-23 14:34:05 -0700

    optional yaml folder configuration

--- lib/add_symbolic_names/railtie.rb
+++ lib/add_symbolic_names/railtie.rb
@@ -2,9 +2,13 @@ module AddSymbolicNames
   class Railtie < ::Rails::Railtie
     config.add_symbolic_names = ActiveSupport::OrderedOptions.new
     config.add_symbolic_names.default_store = nil
+    config.add_symbolic_names.yaml_folder = nil
 
     initializer 'add_symbolic_names' do |app|
       AddSymbolicNames.default_store = app.config.add_symbolic_names.default_store if app.config.add_symbolic_names.default_store
+      AddSymbolicNames::Store::Yaml.folder = app.config.add_symbolic_names.yaml_folder || 
+        File.join(config.root, "db/domain")
+
       ActiveSupport.on_load(:active_record) do
         include AddSymbolicNames::Mixin
       end
--- lib/add_symbolic_names/store/yaml.rb
+++ lib/add_symbolic_names/store/yaml.rb
@@ -3,8 +3,10 @@ require 'yaml'
 module AddSymbolicNames
   module Store
     class Yaml
+      mattr_accessor :folder
+
       def self.define_constants(klass, opts)
-        YAML.load_file(File.join(Rails.root, "db/domain/#{klass.name.demodulize.tableize}.yml")).each do |datum|
+        YAML.load_file(File.join(self.folder, "#{klass.name.demodulize.tableize}.yml")).each do |datum|
           klass.const_set(datum['symbolic_name'], datum['id']) unless klass.const_defined?(datum['symbolic_name'])
         end
       end
